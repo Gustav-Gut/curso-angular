0c83c2b7dcd1604b0f9363687d0fb88a86de64a5
Gustavo Gutierrez <gustavo.gutierrezy@gmail.com>
2020-10-01 15:45:57 -0300

    [CHG] new changes to scopes and father/son component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myProject';
+export class AppComponent implements OnInit{
+  public title: string;
+
+  constructor() { 
+    var cadena: string = '1234';
+    console.log('constructor app-component');
+  }
+
+  ngOnInit() {
+    this.title = 'Título componente padre';
+    console.log('onInit app-component --> ', this.title);
+  }
+
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './components/xyz/xyz.component';
+import { ScopesComponent } from './components/scopes/scopes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    ScopesComponent
   ],
   imports: [
     BrowserModule,
